5954cfdf24da4288271bd7982a086904cc9fba5b
Captain-not-so-obvious <fillipemoreira979@gmail.com>
2024-11-11 16:58:29 -0300

    Adicionei função 3 e 4 ao projeto agenda

--- agenda.rb
+++ agenda.rb
@@ -23,6 +23,39 @@ def adicionar_contato
     @agenda << {nome: nome, telefone: telefone}
 end
 
+def ver_contato
+    print "Qual nome você deseja? "
+    nome = gets.chomp
+
+    @agenda.each do |contato|
+        if contato[:nome].downcase.include?(nome.downcase)
+            puts "#{contato[:nome]} - #{contato[:telefone]}"
+        end
+    end
+                puts "-----------------------------------------"
+end
+
+def editar_contato
+    print "Qual nome deseja editar? "
+    nome = gets.chomp
+
+    @agenda.each do |contato|
+        if contato[:nome].downcase == (nome.downcase)
+            print "Nome para editar ( Se quiser manter o mesmo nome, aperte Enter ): "
+            nome_salvo = contato[:telefone]
+
+           contato[:nome] = gets.chomp
+           contato[:nome] =  contato[:nome].empty? ? nome_salvo : contato[:nome]
+
+           print "Telefone para editar ( Se quiser manter o mesmo telefone, aperte Enter ): "
+           telefone_salvo = contato[:telefone]
+
+           contato[:telefone] = gets.chomp
+           contato[:telefone] = contato[:telefone].empty? ? telefone_salvo : contato[:telefone]
+        end
+    end
+end
+
 loop do
 
 puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4 Editar Contato\n5 Remover Contato\n0. Sair"
@@ -34,7 +67,11 @@ puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4 Editar Contato\n5 Rem
         break
     when codigo == 1
         todos_contatos
-    when codigo = 2
+    when codigo == 2
         adicionar_contato
+    when codigo == 3
+        ver_contato
+    when codigo == 4
+        editar_contato
     end
 end
